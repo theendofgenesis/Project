06f69f09c0dbf4da1987b755c007e1e28c774c9f
comfychair219 <comfychair219@gmail.com>
2016-11-28 19:29:41 -0600

    Update Xmlinput.java
    this does not work, do not commit this

--- SOC Advisor/src/xmlreader/Xmlinput.java
+++ SOC Advisor/src/xmlreader/Xmlinput.java
@@ -6,9 +6,9 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import application.Main;
 
 public class Xmlinput {
 
@@ -17,20 +17,26 @@ public class Xmlinput {
 	public static Document doc;
 	public static XPathExpression expr;
 
+
+	//static String catstring = (Main.catChk.getText());
+	//static int catalogYear = Integer.parseInt(Main.catChk.getText());
+
 	public static void createReader() throws Exception {
 
 
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true); // never forget this!
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse("src\\xmlreader\\coursecatalog.xml");
+        Document doc = builder.parse("src\\xmlreader\\courseCatalog.xml");
 
         XPathFactory xpathfactory = XPathFactory.newInstance();
         XPath xpath = xpathfactory.newXPath();
 
         System.out.println("Get department and number from catalog year 2012");
 
-        XPathExpression expr = xpath.compile("//catalog[@year=2012]/course/department/text() | //catalog[@year=2012]/course/number/text()");
+
+
+        XPathExpression expr = xpath.compile("//core[@year=20122013]/course/title/text() | //core[@year=20122013]/course/title/text()");
         Object result = expr.evaluate(doc, XPathConstants.NODESET);
         nodes = (NodeList) result;
         for (int i = 0; i < nodes.getLength(); i++) {
@@ -54,7 +60,9 @@ public class Xmlinput {
 			XPathFactory xpathfactory = XPathFactory.newInstance();
 	        XPath xpath = xpathfactory.newXPath();
 
-			expr= xpath.compile("//catalog[@year=2012]/course/department/text()");
+
+
+			expr= xpath.compile("//core[@year=20122013]/course/title/text()");
 	        for (int i = 0; i < nodes.getLength(); i++) {
 	            System.out.println("from classList: " + nodes.item(i).getNodeValue());
 
